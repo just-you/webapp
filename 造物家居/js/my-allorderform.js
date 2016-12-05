d981ffbb4a0e4b092dff45f39e910ae2d9737328
just-you <2454746872@qq.com>
2016-12-05 14:16:04 +0800

    update

--- 造物家居/js/my-allorderform.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('.my-menu ul li').mousedown(function(){
-		$(this).addClass('border-bottom').siblings().removeClass('border-bottom');
-		var index=$(this).index();
-		$('.my-nav dd').eq(index).css('display','block').siblings().css('display','none');
-	})
-})
